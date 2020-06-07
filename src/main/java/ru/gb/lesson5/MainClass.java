443f491e27df925291e7a4f3ce793ca9b58e8a0f
Dmitry Lytov <38041284+Dmitrycmc@users.noreply.github.com>
2020-06-07 07:22:09 +0300

    Lesson5 (#6)

---
+++ src/main/java/ru/gb/lesson5/MainClass.java
@@ -0,0 +1,36 @@
+package ru.gb.lesson5;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Employee[] employees = {
+                new Employee(
+                        "Ivanov Ivan", "Engineer", "ivanov@email.ru",
+                        "892312312", 120000, 27
+                ),
+                new Employee(
+                        "Petrov Petr", "Front-end developer", "petrov@email.ru",
+                        "891232312", 120000, 35
+                ),
+                new Employee(
+                        "Romanov Roman", "Back-end developer", "romanov@email.ru",
+                        "898912341", 120000, 45
+                ),
+                new Employee(
+                        "Fyodorov Fyodor", "Sales", "fyodorov@email.ru",
+                        "892377586", 120000, 50
+                ),
+                new Employee(
+                        "Sidorov Sidr", "SEO", "sidorov@email.ru",
+                        "892374125", 120000, 40
+                )
+        };
+
+        for (Employee e : employees) {
+            if (e.getAge() > 40) {
+                e.printInfo();
+            }
+        }
+    }
+}
+
+
