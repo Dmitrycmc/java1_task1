c54c95ae2c231a200942ac9182f8fd60b8942233
Dmitry Lytov <dmitrycmc@gmail.com>
2020-06-08 09:17:46 +0300

    Added counters

--- src/main/java/ru/gb/lesson6/Cat.java
+++ src/main/java/ru/gb/lesson6/Cat.java
@@ -1,13 +1,16 @@
 package ru.gb.lesson6;
 
 public class Cat extends Animal {
+    private static int counter = 0;
+
     public Cat(String name) {
         super(name, 200, 0);
+        System.out.println("Создано котов: " + ++counter);
     }
 
     @Override
     public void info() {
-        System.out.print("Cat ");
+        System.out.print("Кот ");
         super.info();
     }
 }
--- src/main/java/ru/gb/lesson6/Dog.java
+++ src/main/java/ru/gb/lesson6/Dog.java
@@ -1,13 +1,16 @@
 package ru.gb.lesson6;
 
 public class Dog extends Animal {
+    private static int counter = 0;
+
     public Dog(String name) {
         super(name, 500, 10);
+        System.out.println("Создано собак: " + ++counter);
     }
 
     @Override
     public void info() {
-        System.out.print("Dog ");
+        System.out.print("Пес ");
         super.info();
     }
 }
