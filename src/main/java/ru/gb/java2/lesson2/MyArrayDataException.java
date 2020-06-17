0e6bdff332670d0561c577c3d6b62ac17d5687b2
Dmitry Lytov <dmitrycmc@gmail.com>
2020-06-17 20:18:31 +0300

    Fix

--- src/main/java/ru/gb/java2/lesson2/MyArrayDataException.java
+++ src/main/java/ru/gb/java2/lesson2/MyArrayDataException.java
@@ -1,15 +1,7 @@
 package ru.gb.java2.lesson2;
 
 class MyArrayDataException extends RuntimeException {
-    private int i, j;
-
     MyArrayDataException(int i, int j) {
-        this.i = i;
-        this.j = j;
-    }
-
-    @Override
-    public String getMessage() {
-        return "Element (" + i + "; " + j + ") has wrong format";
+        super("Element (" + i + "; " + j + ") has wrong format");
     }
 }
--- src/main/java/ru/gb/java2/lesson2/MyArraySizeException.java
+++ src/main/java/ru/gb/java2/lesson2/MyArraySizeException.java
@@ -1,8 +1,7 @@
 package ru.gb.java2.lesson2;
 
 public class MyArraySizeException extends RuntimeException {
-    @Override
-    public String getMessage() {
-        return "Array has wrong size";
+    MyArraySizeException() {
+        super("Array has wrong size");
     }
 }
