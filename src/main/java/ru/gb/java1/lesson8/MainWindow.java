bf4918dc6fc23b1438cdca6c341bd9c33576735f
Dmitry Lytov <dmitrycmc@gmail.com>
2020-06-18 15:58:28 +0300

    Ready for review

--- src/main/java/ru/gb/java1/lesson8/MainWindow.java
+++ src/main/java/ru/gb/java1/lesson8/MainWindow.java
@@ -6,36 +6,44 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 class MainWindow extends JFrame {
+    private JPanel buttonsPanel = new JPanel();
+    ;
+
+    private JButton startButton = new JButton("<html><body><b style='font-size: 1.5em'>Начать игру</b></body></html>");
+    private JButton exitButton = new JButton("Выйти");
+
+    private SettingsWindow settingsWindow = new SettingsWindow(this);
+
     MainWindow() {
         setTitle("Крестики-нолики");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setBounds(300, 300, 400, 400);
 
-        JPanel buttonsPanel = new JPanel();
-        JButton startButton = new JButton("Начать игру");
-        JButton exitButton = new JButton("Закончить игру");
+        setLayout(new BorderLayout());
+        add(buttonsPanel, BorderLayout.SOUTH);
 
         buttonsPanel.setLayout(new GridLayout(1, 2));
         buttonsPanel.add(startButton);
         buttonsPanel.add(exitButton);
 
-        setLayout(new BorderLayout());
-        add(buttonsPanel, BorderLayout.SOUTH);
-
-        final SettingsWindow settingsWindow = new SettingsWindow(this);
-
-        startButton.addActionListener ( new ActionListener() {
+        startButton.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed ( ActionEvent e ) {
+            public void actionPerformed(ActionEvent e) {
                 settingsWindow.setVisible(true);
             }
         });
 
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
 
         setVisible(true);
     }
 
-    void startGame(boolean gameMode) {
-        System.out.println(gameMode);
+    void startGame(GameMode gameMode, int fieldSize, int winLine) {
+        System.out.println(gameMode + " " + fieldSize + " " + winLine);
     }
 }
--- src/main/java/ru/gb/java1/lesson8/SettingsWindow.java
+++ src/main/java/ru/gb/java1/lesson8/SettingsWindow.java
@@ -1,36 +1,72 @@
 package ru.gb.java1.lesson8;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import static ru.gb.java1.lesson8.GameMode.HvsAI;
+import static ru.gb.java1.lesson8.GameMode.HvsH;
+
 class SettingsWindow extends JFrame {
+    private final static int MIN_FIELD_SIZE = 3;
+    private final static int MAX_FIELD_SIZE = 10;
+
+    private JLabel gameModeLabel = new JLabel("Game mode:");
+    private JRadioButton hvsaiRadio = new JRadioButton("Human vs. AI");
+    private JRadioButton hvshRadio = new JRadioButton("Human vs. human");
+    private ButtonGroup gameModeGroup = new ButtonGroup();
+
+    private JLabel fieldSizeLabel = new JLabel("Field size: " + MIN_FIELD_SIZE);
+    private JSlider fieldSizeSlider = new JSlider(MIN_FIELD_SIZE, MAX_FIELD_SIZE, MIN_FIELD_SIZE);
+    private JLabel winLineLabel = new JLabel("Win line: " + MIN_FIELD_SIZE);
+    private JSlider winLineSlider = new JSlider(MIN_FIELD_SIZE, MAX_FIELD_SIZE, MIN_FIELD_SIZE);
+
+    private JButton startButton = new JButton("Начать");
+
     SettingsWindow(final MainWindow mainWindow) {
         setTitle("Новая игра");
         setBounds(300, 300, 400, 400);
 
-        JLabel gameModeLabel = new JLabel("Game mode:");
-        final JRadioButton hvsaiRadio = new JRadioButton("Human vs. AI");
-        JRadioButton hvshRadio = new JRadioButton("Human vs. human");
-        hvsaiRadio.setSelected(true);
-        ButtonGroup gameModeGroup = new ButtonGroup();
-        gameModeGroup.add(hvsaiRadio);
-        gameModeGroup.add(hvshRadio);
-
-        JButton startButton = new JButton("Начать");
-
-        setLayout(new GridLayout(5, 1));
+        setLayout(new GridLayout(10, 1));
         add(gameModeLabel);
         add(hvsaiRadio);
         add(hvshRadio);
+        add(fieldSizeLabel);
+        add(fieldSizeSlider);
+        add(winLineLabel);
+        add(winLineSlider);
         add(startButton);
 
-        startButton.addActionListener ( new ActionListener() {
+        hvsaiRadio.setSelected(true);
+        gameModeGroup.add(hvsaiRadio);
+        gameModeGroup.add(hvshRadio);
+
+        startButton.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed ( ActionEvent e ) {
-                mainWindow.startGame(hvsaiRadio.isSelected());
+            public void actionPerformed(ActionEvent e) {
+                GameMode gameMode = hvsaiRadio.isSelected() ? HvsAI : HvsH;
+                mainWindow.startGame(gameMode, fieldSizeSlider.getValue(), winLineSlider.getValue());
                 setVisible(false);
+            }
+        });
+
+        fieldSizeSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                int currentValue = fieldSizeSlider.getValue();
+                fieldSizeLabel.setText("Field size: " + currentValue);
+                winLineSlider.setMaximum(currentValue);
+            }
+        });
+
+        winLineSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                int currentValue = winLineSlider.getValue();
+                winLineLabel.setText("Win line: " + currentValue);
             }
         });
     }
