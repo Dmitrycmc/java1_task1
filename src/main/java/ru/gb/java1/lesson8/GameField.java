4b3c14ecce3b004e4e467fbb825da0c9d01448b1
Dmitry Lytov <dmitrycmc@gmail.com>
2020-06-20 19:50:09 +0300

    Started working on game field planel

---
+++ src/main/java/ru/gb/java1/lesson8/GameField.java
@@ -0,0 +1,23 @@
+package ru.gb.java1.lesson8;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameField extends JPanel {
+    private JButton[][] buttons;
+
+    public void startGame(GameMode gameMode, int fieldSize, int winLine) {
+        setLayout(new GridLayout(fieldSize, fieldSize));
+
+        buttons = new JButton[fieldSize][fieldSize];
+
+        for (JButton[] buttonsLine: buttons) {
+            for (JButton button: buttonsLine) {
+                button = new JButton("button");
+                add(button);
+            }
+        }
+
+        // Эти кнопки не отображаются!
+    }
+}
--- src/main/java/ru/gb/java1/lesson8/SettingsWindow.java
+++ src/main/java/ru/gb/java1/lesson8/SettingsWindow.java
@@ -22,13 +22,15 @@ class SettingsWindow extends JFrame {
     private JLabel fieldSizeLabel = new JLabel("Field size: " + MIN_FIELD_SIZE);
     private JSlider fieldSizeSlider = new JSlider(MIN_FIELD_SIZE, MAX_FIELD_SIZE, MIN_FIELD_SIZE);
     private JLabel winLineLabel = new JLabel("Win line: " + MIN_FIELD_SIZE);
-    private JSlider winLineSlider = new JSlider(MIN_FIELD_SIZE, MAX_FIELD_SIZE, MIN_FIELD_SIZE);
+    private JSlider winLineSlider = new JSlider(MIN_FIELD_SIZE, MIN_FIELD_SIZE, MIN_FIELD_SIZE);
 
     private JButton startButton = new JButton("Начать");
 
     SettingsWindow(final MainWindow mainWindow) {
         setTitle("Новая игра");
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         setBounds(300, 300, 400, 400);
+        setResizable(false);
 
         setLayout(new GridLayout(10, 1));
         add(gameModeLabel);
@@ -49,6 +51,7 @@ class SettingsWindow extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 GameMode gameMode = hvsaiRadio.isSelected() ? HvsAI : HvsH;
                 mainWindow.startGame(gameMode, fieldSizeSlider.getValue(), winLineSlider.getValue());
+                mainWindow.setEnabled(true);
                 setVisible(false);
             }
         });
