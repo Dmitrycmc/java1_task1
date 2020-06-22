fe5a966b8844e78e10ffb2107e792510a63227e2
Dmitry Lytov <dmitrycmc@gmail.com>
2020-06-22 05:17:06 +0300

    Started working

---
+++ src/main/java/ru/gb/java2/lesson4/Controller.java
@@ -0,0 +1,4 @@
+package ru.gb.java2.lesson4;
+
+public class Controller {
+}
---
+++ src/main/java/ru/gb/java2/lesson4/Main.java
@@ -0,0 +1,24 @@
+package ru.gb.java2.lesson4;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        System.out.println(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/ru/gb/java2/lesson4/sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
