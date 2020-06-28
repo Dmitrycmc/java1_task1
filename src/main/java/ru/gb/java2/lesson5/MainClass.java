bf1acf42b0a01ab5b8315443fff77dd82ef01e86
Dmitry Lytov <dmitrycmc@gmail.com>
2020-06-28 05:59:27 +0300

    Codestyle fixed

--- src/main/java/ru/gb/java2/lesson5/MainClass.java
+++ src/main/java/ru/gb/java2/lesson5/MainClass.java
@@ -20,7 +20,7 @@ public class MainClass {
         long t0 = System.currentTimeMillis();
 
         for (int i = 0; i < SIZE; i++) {
-            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
 
         long t1 = System.currentTimeMillis();
@@ -44,8 +44,12 @@ public class MainClass {
         System.arraycopy(arr, 0, arr1, 0, halfSize);
         System.arraycopy(arr, halfSize, arr2, 0, SIZE - halfSize);
 
-        ArrayTransformer transformer1 = new ArrayTransformer(arr1, (v, i) -> (float)(v * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2)));
-        ArrayTransformer transformer2 = new ArrayTransformer(arr2, (v, i) -> (float)(v * Math.sin(0.2f + (halfSize + i) / 5) * Math.cos(0.2f + (halfSize + i) / 5) * Math.cos(0.4f + (halfSize + i) / 2)));
+        ArrayTransformer transformer1 = new ArrayTransformer(arr1,
+                (v, i) -> (float) (v * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2))
+        );
+        ArrayTransformer transformer2 = new ArrayTransformer(arr2,
+                (v, i) -> (float) (v * Math.sin(0.2f + (halfSize + i) / 5) * Math.cos(0.2f + (halfSize + i) / 5) * Math.cos(0.4f + (halfSize + i) / 2))
+        );
 
         Thread thread1 = new Thread(transformer1);
         Thread thread2 = new Thread(transformer2);
