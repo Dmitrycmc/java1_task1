668f2a78f81f0a8d38b21a44a7e52031cc2ba33a
Dmitry Lytov <dmitrycmc@gmail.com>
2020-06-29 11:20:52 +0300

    Improved

--- src/main/java/ru/gb/java2/lesson6/Client.java
+++ src/main/java/ru/gb/java2/lesson6/Client.java
@@ -4,60 +4,26 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-import java.util.Scanner;
 
-public class Client {
+class Client extends Participant {
     private static Socket socket;
-    private static DataInputStream in;
-    private static DataOutputStream out;
 
     public static void main(String[] args) {
         start("localhost", 3030);
     }
 
-    public static void start(String domain, int port) {
+    private static void start(String domain, int port) {
         try {
             socket = new Socket(domain, port);
+            System.out.println("Соединение установлено!");
             in = new DataInputStream(socket.getInputStream());
             out = new DataOutputStream(socket.getOutputStream());
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Не удалось подключиться!");
+            System.exit(0);
         }
 
-        listenResponse();
+        listen();
         sendingFromConsole();
-    }
-
-    private static void sendingFromConsole() {
-        Scanner scanner = new Scanner(System.in);
-        String msg;
-        while (!(msg = scanner.nextLine()).equalsIgnoreCase("/end")) {
-            send(msg);
-        }
-    }
-
-    public static void listenResponse() {
-        new Thread(() -> {
-            try {
-                while (true) {
-                    String incomeMessage = in.readUTF();
-                    if (incomeMessage.equalsIgnoreCase("/end")) {
-                        break;
-                    }
-                    System.out.println("Принято сообщение: " + incomeMessage);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }).start();
-    }
-
-    public static void send(String str) {
-        try {
-            out.writeUTF(str);
-            System.out.println("Отправлено сообщение: " + str);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
--- src/main/java/ru/gb/java2/lesson6/Server.java
+++ src/main/java/ru/gb/java2/lesson6/Server.java
@@ -5,65 +5,26 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
 
-public class Server {
+class Server extends Participant {
     private static Socket socket;
     private static ServerSocket serverSocket;
-    private static DataInputStream in;
-    private static DataOutputStream out;
-
 
     public static void main(String[] args) {
         start(3030);
     }
 
-    private static void listenRequest() {
-        new Thread(() -> {
-            try {
-                while (true) {
-                    String str = in.readUTF();
-                    if (str.equalsIgnoreCase("/end")) {
-                        break;
-                    }
-                    System.out.println("Принято сообщение: " + str);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }).start();
-    }
-
-    private static void sendingFromConsole() {
-        Scanner scanner = new Scanner(System.in);
-        String msg;
-        while (!(msg = scanner.nextLine()).equalsIgnoreCase("/end")) {
-            send(msg);
-        }
-    }
-
-    public static void send(String str) {
-        try {
-            out.writeUTF(str);
-            System.out.println("Отправлено сообщение: " + str);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void start(int port) {
+    static void start(int port) {
         try {
             serverSocket = new ServerSocket(port);
             System.out.println("Сервер запущен, ожидаем подключения...");
             socket = serverSocket.accept();
-
             System.out.println("Клиент подключился");
-            System.out.println(socket.toString());
 
             in = new DataInputStream(socket.getInputStream());
             out = new DataOutputStream(socket.getOutputStream());
 
-            listenRequest();
+            listen();
             sendingFromConsole();
         } catch (IOException e) {
             e.printStackTrace();
