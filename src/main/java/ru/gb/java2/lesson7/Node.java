db38d379564e20d981c39868aa6d5ca6e946e155
Dmitry Lytov <38041284+Dmitrycmc@users.noreply.github.com>
2020-07-07 09:36:03 +0300

    Lesson7 (#17)
    * Inserting implemented
    
    * Removing implemented
    
    * OneDirList all methods implemented
    
    * TwoDirList some methods implemented
    
    * TwoDirList all methods implemented
    
    * TwoDirList validation implemented
    
    * Some fixes
    
    * Array constructor implemented
    
    * Classes became package-private

---
+++ src/main/java/ru/gb/java2/lesson7/Node.java
@@ -0,0 +1,5 @@
+package ru.gb.java2.lesson7;
+
+interface Node<T> {
+    T getValue();
+}
---
+++ src/main/java/ru/gb/java2/lesson7/OneDirectionalList.java
@@ -0,0 +1,126 @@
+package ru.gb.java2.lesson7;
+
+class OneDirectionalList<T> implements DirectionalList<T> {
+    private OneDirectionalNode<T> emptyHeadNode;
+    private int size;
+
+    OneDirectionalList(T[] arr) {
+        emptyHeadNode = new OneDirectionalNode<>(null, null);
+        OneDirectionalNode<T> current = emptyHeadNode;
+        for (int i = 0; i < arr.length; i++) {
+            current.next = new OneDirectionalNode<>(arr[i], null);
+            current = current.next;
+        }
+        size = arr.length;
+    }
+
+    OneDirectionalList() {
+        emptyHeadNode = new OneDirectionalNode<>(null, null);
+        size = 0;
+    }
+
+    @Override
+    public OneDirectionalNode insertToStart(T value) throws Exception {
+        return insertAt(value, 0);
+    }
+
+    @Override
+    public OneDirectionalNode insertAt(T value, int index) throws Exception {
+        OneDirectionalNode<T> current = emptyHeadNode;
+        for (int i = 0; i < index; i++) {
+            if (current.getNext() == null) {
+                throw new Exception();
+            }
+            current = current.getNext();
+        }
+        size++;
+        return current.next = new OneDirectionalNode<>(value, current.next);
+    }
+
+    @Override
+    public boolean remove(T value) {
+        OneDirectionalNode<T> current = emptyHeadNode;
+        while (current.next != null) {
+            if (current.next.value.equals(value)) {
+                current.next = current.next.next;
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    @Override
+    public void removeAt(int index) throws Exception {
+        OneDirectionalNode<T> current = emptyHeadNode;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+            if (current == null) {
+                throw new Exception();
+            }
+        }
+        if (current.next == null) {
+            throw new Exception();
+        }
+        size--;
+        current.next = current.next.next;
+    }
+
+    @Override
+    public OneDirectionalNode getFirst() throws Exception {
+        return getAt(0);
+    }
+
+    @Override
+    public OneDirectionalNode getAt(int index) throws Exception {
+        OneDirectionalNode<T> current = emptyHeadNode;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+            if (current == null) {
+                throw new Exception();
+            }
+        }
+        return current.next;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Length = ").append(getSize()).append(" [ ");
+        OneDirectionalNode<T> current = emptyHeadNode;
+        if (current.next != null) {
+            current = current.next;
+            sb.append(current.getValue()).append(" ");
+        }
+        while (current.next != null) {
+            current = current.next;
+            sb.append(", ").append(current.getValue()).append(" ");
+        }
+        return sb.append("]").toString();
+    }
+
+    public class OneDirectionalNode<T> implements Node<T> {
+        private T value;
+        private OneDirectionalNode<T> next;
+
+        OneDirectionalNode(T value, OneDirectionalNode<T> next) {
+            this.value = value;
+            this.next = next;
+
+        }
+
+        @Override
+        public T getValue() {
+            return value;
+        }
+
+        OneDirectionalNode<T> getNext() {
+            return next;
+        }
+    }
+}
