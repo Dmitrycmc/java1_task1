55a74399a7a07b156022504ef870fb5093080f45
Dmitry Lytov <38041284+Dmitrycmc@users.noreply.github.com>
2020-08-03 04:33:17 +0300

    Chat duplicate (#20)
    * Chat code duplicated

---
+++ src/main/java/ru/gb/java3/lesson2/src/main/java/client/ClientApplication.java
@@ -0,0 +1,9 @@
+package client;
+
+public class ClientApplication {
+    public static void main(String[] args) {
+        new Client("localhost", 8589);
+    }
+}
+
+
---
+++ src/main/java/ru/gb/java3/lesson2/src/main/java/server/Server.java
@@ -0,0 +1,64 @@
+package server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedList;
+
+class Server {
+    private int port;
+    private MainWindow mainWindow;
+    private LinkedList<ClientHandler> clientHandlers = new LinkedList<>();
+
+    Server(int port) {
+        this.port = port;
+        start();
+    }
+
+    void broadcast(String message) {
+        for (ClientHandler clientHandler : clientHandlers) {
+            clientHandler.send(message);
+        }
+    }
+
+    void privateMessage(String userLoginFrom, String userLoginTo, String message) {
+        for (ClientHandler clientHandler : clientHandlers) {
+            if (clientHandler.getLogin().equals(userLoginFrom) || clientHandler.getLogin().equals(userLoginTo))
+                clientHandler.send(message);
+        }
+    }
+
+    private void start() {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Сервер запущен");
+        mainWindow = new MainWindow(clientHandlers);
+
+        while (true) {
+            try {
+                System.out.println("Ожидаем подключения...");
+                Socket socket = serverSocket.accept();
+                System.out.println("Клиент подключился: " + socket);
+                DataInputStream in = new DataInputStream(socket.getInputStream());
+                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+                ClientHandler clientHandler = new ClientHandler(in, out, this, mainWindow);
+                new Thread(() -> {
+                    clientHandler.handleClient();
+                    clientHandlers.remove(clientHandler);
+                    mainWindow.refreshClients();
+                }).start();
+                clientHandlers.add(clientHandler);
+                mainWindow.refreshClients();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
