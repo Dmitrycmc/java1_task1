15d89711a460a5a073f55815f7051a070fa93b3f
Dmitry Lytov <dmitrycmc@gmail.com>
2020-08-03 04:31:16 +0300

    Chat code duplicated

---
+++ src/main/java/ru/gb/java3/lesson2/src/main/java/server/MainWindow.java
@@ -0,0 +1,31 @@
+package server;
+
+import javax.swing.*;
+import java.util.LinkedList;
+
+class MainWindow extends JFrame {
+    private LinkedList<ClientHandler> clientHandlers;
+    private JTextArea clientsArea;
+
+    MainWindow(LinkedList<ClientHandler> clientHandlers) {
+        this.clientHandlers = clientHandlers;
+
+        setTitle("Сервер чата");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(300, 300, 400, 400);
+        setResizable(false);
+
+        clientsArea = new JTextArea();
+        clientsArea.setEditable(false);
+        add(clientsArea);
+
+        setVisible(true);
+    }
+
+    void refreshClients() {
+        clientsArea.setText("");
+        for (ClientHandler clientHandler : clientHandlers) {
+            clientsArea.setText(clientsArea.getText() + (clientsArea.getText().equals("") ? "" : '\n') + clientHandler.getLogin());
+        }
+    }
+}
