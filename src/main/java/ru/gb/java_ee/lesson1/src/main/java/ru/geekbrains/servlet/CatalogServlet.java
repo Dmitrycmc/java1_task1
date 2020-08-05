338f7b998a17dee8ae7b630b43b5089bfb9119cc
Dmitry Lytov <dmitrycmc@gmail.com>
2020-08-05 12:35:19 +0300

    Added menu

--- src/main/java/ru/gb/java_ee/lesson1/src/main/java/ru/geekbrains/servlet/CatalogServlet.java
+++ src/main/java/ru/gb/java_ee/lesson1/src/main/java/ru/geekbrains/servlet/CatalogServlet.java
@@ -1,5 +1,7 @@
 package ru.geekbrains.servlet;
 
+import ru.geekbrains.template.Menu;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +13,10 @@ import java.io.IOException;
 public class CatalogServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("<h1>Каталог</h1>");
+        resp.getWriter().println(render());
+    }
+
+    private String render() {
+        return Menu.render() + "<h1>Каталог</h1>";
     }
 }
