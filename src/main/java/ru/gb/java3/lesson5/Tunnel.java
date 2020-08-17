f86164497aef0fbb6312da7273ea8b9140b370e3
Dmitry Lytov <dmitrycmc@gmail.com>
2020-08-17 06:58:23 +0300

    Synchronized

--- src/main/java/ru/gb/java3/lesson5/Tunnel.java
+++ src/main/java/ru/gb/java3/lesson5/Tunnel.java
@@ -1,21 +1,29 @@
 package ru.gb.java3.lesson5;
 
+import java.util.concurrent.Semaphore;
+
 public class Tunnel extends Stage {
-    public Tunnel() {
-        this.length = 80;
+    Semaphore semaphore;
+
+    public Tunnel(int length, int carsRestriction) {
+        this.length = length;
+        this.semaphore = new Semaphore(carsRestriction);
         this.description = "Тоннель " + length + " метров";
     }
+
     @Override
     public void go(Car c) {
         try {
             try {
                 System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                semaphore.acquire();
                 System.out.println(c.getName() + " начал этап: " + description);
                 Thread.sleep(length / c.getSpeed() * 1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
                 System.out.println(c.getName() + " закончил этап: " + description);
+                semaphore.release();
             }
         } catch (Exception e) {
             e.printStackTrace();
