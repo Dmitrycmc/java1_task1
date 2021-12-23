b94ac3abfdc5f6a560a15710aa8cb65c661b88b8
Dmitry Lytov <dmitrycmc@yandex-team.ru>
2021-12-23 19:24:53 +0300

    StarGame-0

---
+++ star-game/desktop/src/ru/gb/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package ru.gb.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import game.BallGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1280;
+		config.height = 720;
+		new LwjglApplication(new BallGame(), config);
+	}
+}
