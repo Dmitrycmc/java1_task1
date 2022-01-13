7c8a81bc8a33892ea370782d144a9cfa061ede3f
Dmitry Lytov <dmitrycmc@yandex-team.ru>
2022-01-13 06:59:17 +0300

    Revert "Spring boot"
    This reverts commit 002e1f124151e15f4c5e840ffc30576cb27e9b9c.

--- spring1/lesson4-spring-boot/src/main/java/ru/geekbrains/lesson4/controller/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.geekbrains.lesson4.controller;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
