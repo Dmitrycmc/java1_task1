51a52f3f4b1febef5b4a6ad30bbe2e69ce63055c
Dmitry Lytov <38041284+Dmitrycmc@users.noreply.github.com>
2022-01-17 20:27:54 +0300

    Spring1 lesson5 (#49)
    * Added new module
    
    * Done
    
    * Refactored
    
    * Rolled back
    
    Co-authored-by: Dmitry Lytov <dmitrycmc@yandex-team.ru>

---
+++ spring1/lesson-05-hibernate/src/main/java/ru/geekbrains/entity/ProductDaoImpl.java
@@ -0,0 +1,69 @@
+package ru.geekbrains.entity;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import java.util.List;
+import java.util.Optional;
+
+public class ProductDaoImpl implements ProductDao {
+    EntityManagerFactory emFactory;
+
+    private interface MyInterface<T> {
+        T doTx(EntityManager em);
+    }
+
+    private <T> T execInTx(MyInterface<T> i) {
+        EntityManager em = emFactory.createEntityManager();
+        em.getTransaction().begin();
+        try {
+            T result = i.doTx(em);
+            em.getTransaction().commit();
+            return result;
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+        return null;
+    }
+
+    private <T> T exec(MyInterface<T> i) {
+        EntityManager em = emFactory.createEntityManager();
+        T result = i.doTx(em);
+        em.close();
+        return result;
+    }
+
+    public ProductDaoImpl(EntityManagerFactory emFactory) {
+        this.emFactory = emFactory;
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return exec(em -> em.createQuery("select p from Product p", Product.class).getResultList());
+    }
+
+    @Override
+    public Optional<Product> findById(long id) {
+        Product p = exec(em -> em.find(Product.class, id));
+
+        return Optional.ofNullable(p);
+    }
+
+    @Override
+    public void save(Product product) {
+        if (product.getId() != null && findById(product.getId()).isPresent()) {
+            execInTx(em -> em.merge(product));
+        } else {
+            execInTx(em -> {em.persist(product); return product;});
+        }
+    }
+
+    @Override
+    public void delete(long id) {
+        execInTx(em -> em.createQuery("delete from Product where id = :id")
+            .setParameter("id", id)
+            .executeUpdate()
+        );
+    }
+}
