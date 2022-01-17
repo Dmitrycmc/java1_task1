8459583ff5fbb3c657be487eb9c4def4eb245126
Dmitry Lytov <38041284+Dmitrycmc@users.noreply.github.com>
2022-01-17 20:18:33 +0300

    Spring1 lesson4 (#48)
    * spring1-lesson4
    
    * Added validation
    
    Co-authored-by: Dmitry Lytov <dmitrycmc@yandex-team.ru>

---
+++ spring1/lesson4-spring-boot/src/main/java/ru/geekbrains/lesson4/persist/Product.java
@@ -0,0 +1,64 @@
+package ru.geekbrains.lesson4.persist;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+public class Product {
+
+    private Long id;
+
+    @NotBlank(message = "Введите имя")
+    private String name;
+
+    private String description;
+
+    @NotNull(message = "Введите цену")
+    private BigDecimal price;
+
+    public Product() {
+    }
+
+    public Product(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Product(String name, BigDecimal price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
