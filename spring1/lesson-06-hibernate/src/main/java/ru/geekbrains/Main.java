9b0a992de5317c9108970a70f05be067e627deb1
Dmitry Lytov <dmitrycmc@yandex-team.ru>
2022-01-18 04:07:40 +0300

    Codestyle

--- spring1/lesson-06-hibernate/src/main/java/ru/geekbrains/Main.java
+++ spring1/lesson-06-hibernate/src/main/java/ru/geekbrains/Main.java
@@ -1,18 +1,8 @@
 package ru.geekbrains;
 
-import org.hibernate.cfg.Configuration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import ru.geekbrains.entity.customer.Customer;
-import ru.geekbrains.entity.customer.CustomerDao;
-import ru.geekbrains.entity.customer.CustomerDaoImpl;
-import ru.geekbrains.entity.product.Product;
-import ru.geekbrains.entity.product.ProductDao;
-import ru.geekbrains.entity.product.ProductDaoImpl;
 import ru.geekbrains.services.CustomerService;
-
-import javax.persistence.EntityManagerFactory;
-import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) {
