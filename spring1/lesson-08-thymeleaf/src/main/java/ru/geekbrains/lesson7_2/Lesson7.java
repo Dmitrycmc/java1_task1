e877ce2d017e454b482f622243d71bc370dccb10
Dmitry Lytov <38041284+Dmitrycmc@users.noreply.github.com>
2022-01-27 04:10:29 +0300

    Spring1 lesson08 cw (#55)
    * Project copied
    
    * Done
    
    Co-authored-by: Dmitry Lytov <dmitrycmc@yandex-team.ru>

---
+++ spring1/lesson-08-thymeleaf/src/main/java/ru/geekbrains/lesson7_2/Lesson7.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.lesson7_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson7 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson7.class, args);
+    }
+
+}
