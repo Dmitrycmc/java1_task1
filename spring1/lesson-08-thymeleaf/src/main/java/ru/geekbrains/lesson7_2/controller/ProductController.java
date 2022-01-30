6ddbe5e22641cd9970b2214a0282cba03cb75ee4
Dmitry Lytov <38041284+Dmitrycmc@users.noreply.github.com>
2022-01-30 08:28:50 +0300

    Spring1 lesson08 hw (#56)
    * Pagination finished
    
    * Desc sort implemented
    
    * Fixes
    
    * Fix
    
    * Fixes
    
    * Done
    
    * Fix
    
    * Ready for review
    
    Co-authored-by: Dmitry Lytov <dmitrycmc@yandex-team.ru>

--- spring1/lesson-08-thymeleaf/src/main/java/ru/geekbrains/lesson7_2/controller/ProductController.java
+++ spring1/lesson-08-thymeleaf/src/main/java/ru/geekbrains/lesson7_2/controller/ProductController.java
@@ -15,6 +15,8 @@ import ru.geekbrains.lesson7_2.persist.ProductRepository;
 
 import javax.validation.Valid;
 import java.math.BigDecimal;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
 @Controller
@@ -36,7 +38,8 @@ public class ProductController {
         @RequestParam Optional<BigDecimal> maxPriceFilter,
         @RequestParam Optional<Integer> page,
         @RequestParam Optional<Integer> size,
-        @RequestParam Optional<String> sort
+        @RequestParam Optional<String> sort,
+        @RequestParam Optional<Boolean> desc
     ) {
 
         // Первый способ
@@ -53,7 +56,15 @@ public class ProductController {
                 .and(nameFilter.<Specification<Product>>map(s -> (root, query, cb) -> cb.like(root.get("name"), "%" + s + "%")).orElse(null))
                 .and(minPriceFilter.<Specification<Product>>map(s -> (root, query, cb) -> cb.ge(root.get("price"), s)).orElse(null))
                 .and(maxPriceFilter.<Specification<Product>>map(s -> (root, query, cb) -> cb.le(root.get("price"), s)).orElse(null)),
-                PageRequest.of(page.orElse(1) - 1, size.orElse(5), Sort.by(sort.orElse("id"))));
+                PageRequest.of(
+                        page.orElse(1) - 1,
+                        size.orElse(5),
+                        Sort.by(
+                                desc.orElse(false) ? Sort.Direction.DESC : Sort.Direction.ASC,
+                                sort.filter(s -> !s.isEmpty()).orElse("id")
+                        )
+                )
+        );
 
         model.addAttribute("products", products);
         return "product";
@@ -66,10 +77,30 @@ public class ProductController {
         return "product_form";
     }
 
-    @GetMapping("/delete/{id}")
-    public String delete(@PathVariable("id") Long id, Model model) {
+    @DeleteMapping("/{id}")
+    public String delete(
+        @PathVariable("id") Long id,
+        @RequestParam Optional<String> nameFilter,
+        @RequestParam Optional<BigDecimal> minPriceFilter,
+        @RequestParam Optional<BigDecimal> maxPriceFilter,
+        @RequestParam Optional<Integer> page,
+        @RequestParam Optional<Integer> size,
+        @RequestParam Optional<String> sort,
+        @RequestParam Optional<Boolean> desc
+    ) {
         productRepository.deleteById(id);
-        return "redirect:/product";
+        StringBuilder sb = new StringBuilder();
+        sb.append("redirect:/product?");
+        System.out.println(nameFilter);
+        nameFilter.ifPresent(s -> sb.append("&nameFilter=").append(URLEncoder.encode(s)));
+        minPriceFilter.ifPresent(bigDecimal -> sb.append("&minPriceFilter=").append(bigDecimal));
+        maxPriceFilter.ifPresent(bigDecimal -> sb.append("&maxPriceFilter=").append(bigDecimal));
+        page.ifPresent(integer -> sb.append("&page=").append(integer));
+        size.ifPresent(integer -> sb.append("&size=").append(integer));
+        sort.ifPresent(s -> sb.append("&sort=").append(s));
+        desc.ifPresent(aBoolean -> sb.append("&desc=").append(aBoolean));
+        System.out.println(sb.toString());
+        return sb.toString();
     }
 
     @GetMapping("/new")
