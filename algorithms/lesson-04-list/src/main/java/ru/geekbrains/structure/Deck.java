aa6f74861ad9512894ed460f9d255f0da37e4a2c
Dmitry Lytov <dmitrycmc@yandex-team.ru>
2022-03-02 04:38:36 +0300

    Implemented remove & insert

--- algorithms/lesson-04-list/src/main/java/ru/geekbrains/structure/Deck.java
+++ algorithms/lesson-04-list/src/main/java/ru/geekbrains/structure/Deck.java
@@ -70,16 +70,127 @@ public class Deck<T> implements Queue<T>, Stack<T> {
         return node.value;
     }
 
+    private void remove(Node node) {
+        if (node.prev == null) {
+            start = node.next;
+        } else {
+            node.prev.next = node.next;
+        }
+
+        if (node.next == null) {
+            end = node.prev;
+        } else {
+            node.next.prev = node.prev;
+        }
+
+        node.prev = node.next = null;
+    }
+
+    public boolean remove(T el) {
+        Node node = start;
+
+        while (true) {
+            if (node == null) {
+                return false;
+            }
+
+            if (node.value.equals(el)) {
+                remove(node);
+                return true;
+            }
+
+            node = node.next;
+        }
+    }
+
+    public int removeAll(T el) {
+        int counter = 0;
+        while (remove(el)) {
+            counter++;
+        }
+        return counter;
+    }
+
+    public void insertAfter(Node after, T el) {
+        if (after.next == null) {
+            push(el);
+        } else {
+            Node node = new Node();
+            node.value = el;
+            node.prev = after;
+            node.next = after.next;
+            after.next = node;
+            node.next.prev = node;
+        }
+    }
+
+    public boolean insertAfter(T after, T el) {
+        Node node = start;
+
+        while (true) {
+            if (node == null) {
+                return false;
+            }
+
+            if (node.value.equals(after)) {
+                insertAfter(node, el);
+                return true;
+            }
+
+            node = node.next;
+        }
+
+    }
+
+    public void insertBefore(Node before, T el) {
+        if (before.prev == null) {
+            unshift(el);
+        } else {
+            Node node = new Node();
+            node.value = el;
+            node.next = before;
+            node.prev = before.prev;
+            before.prev = node;
+            node.prev.next = node;
+        }
+    }
+
+    public boolean insertBefore(T before, T el) {
+        Node node = start;
+
+        while (true) {
+            if (node == null) {
+                return false;
+            }
+
+            if (node.value.equals(before)) {
+                insertBefore(node, el);
+                return true;
+            }
+
+            node = node.next;
+        }
+
+    }
+
     @Override
     public String toString() {
+        if (start == null) {
+            return "";
+        }
+
         StringBuilder sb = new StringBuilder();
         Node i = start;
 
-        while (i != null) {
-            sb.append(i.value).append(" <-> ");
+        while (true) {
+            sb.append(i.value);
             i = i.next;
-        }
 
-        return sb.toString();
+            if (i == null) {
+                return sb.toString();
+            }
+
+            sb.append(" <-> ");
+        }
     }
 }
--- algorithms/lesson-04-list/src/test/java/StructureTest.java
+++ algorithms/lesson-04-list/src/test/java/StructureTest.java
@@ -4,6 +4,8 @@ import ru.geekbrains.structure.Deck;
 import ru.geekbrains.structure.Queue;
 import ru.geekbrains.structure.Stack;
 
+import java.util.Deque;
+
 public class StructureTest {
     @Test
     void stack() {
@@ -44,5 +46,54 @@ public class StructureTest {
         Assertions.assertEquals(stack.shift(), 3);
         Assertions.assertEquals(stack.shift(), 4);
         Assertions.assertNull(stack.shift());
+    }
+
+    @Test
+    void dequeRemove() {
+        Deck<Integer> deque = new Deck<>();
+
+        Assertions.assertEquals(deque.toString(), "");
+
+        deque.push(2);
+        deque.push(2);
+        deque.push(3);
+        deque.push(4);
+        deque.push(2);
+        deque.push(1);
+        deque.push(2);
+
+        Assertions.assertEquals(deque.toString(), "2 <-> 2 <-> 3 <-> 4 <-> 2 <-> 1 <-> 2");
+
+
+        Assertions.assertTrue(deque.remove(3));
+        Assertions.assertFalse(deque.remove(30));
+        Assertions.assertEquals(deque.toString(), "2 <-> 2 <-> 4 <-> 2 <-> 1 <-> 2");
+
+        Assertions.assertEquals(deque.removeAll(2), 4);
+        Assertions.assertEquals(deque.toString(), "4 <-> 1");
+    }
+
+    @Test
+    void dequeInsert() {
+        Deck<Integer> deque = new Deck<>();
+
+        Assertions.assertEquals(deque.toString(), "");
+
+        deque.push(1);
+        deque.push(2);
+        deque.push(3);
+        deque.push(4);
+        deque.push(3);
+        deque.push(2);
+        deque.push(1);
+
+        Assertions.assertEquals(deque.toString(), "1 <-> 2 <-> 3 <-> 4 <-> 3 <-> 2 <-> 1");
+
+        Assertions.assertTrue(deque.insertAfter(2, 0));
+        Assertions.assertEquals(deque.toString(), "1 <-> 2 <-> 0 <-> 3 <-> 4 <-> 3 <-> 2 <-> 1");
+
+        Assertions.assertTrue(deque.insertBefore(2, 9));
+        Assertions.assertFalse(deque.insertBefore(20, 9));
+        Assertions.assertEquals(deque.toString(), "1 <-> 9 <-> 2 <-> 0 <-> 3 <-> 4 <-> 3 <-> 2 <-> 1");
     }
 }
