440628366484df4016d0eaec9ab76c489b5351f2
Dmitry Lytov <dmitrycmc@yandex-team.ru>
2022-03-07 02:29:26 +0300

    Done

--- algorithms/lesson-05-recursion/src/test/java/TaskTest.java
+++ algorithms/lesson-05-recursion/src/test/java/TaskTest.java
@@ -1,10 +1,15 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.geekbrains.task.Pow;
+import ru.geekbrains.task.Thief;
+import ru.geekbrains.utils.MathUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TaskTest {
     @Test
-    void stringReverse() {
+    void pow() {
         for (int i = -4; i <= 4; i++) {
             for (int j = -4; j <= 4 ; j++) {
                 if (i == 0 && j < 0) {
@@ -18,5 +23,23 @@ public class TaskTest {
                 }
             }
         }
+    }
+
+    @Test
+    void thief() {
+        List<Thief.Item> list = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            list.add(new Thief.Item(
+                    Integer.toString(i),
+                    MathUtils.randomInt(50, 10000),
+                    MathUtils.randomInt(0, 10000)
+            ));
+        }
+
+        for (Thief.Item item : list) {
+            System.out.println(item);
+        }
+
+        System.out.println(Thief.resolve(list, 20000));
     }
 }
